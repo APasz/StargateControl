091261ea03cde29e912d0c437de22da592d42aeb
APasz <a.pasz96@gmail.com>
2025-12-14 10:47:17 +1100

    New Moon address

--- addresses.lua
+++ addresses.lua
@@ -1,6 +1,6 @@
 return {
     { galaxy = "MilkyWay", intergalaxial = {"*"}, site = "Earth", address = { 30, 18, 9, 5, 25, 14, 31, 15, 0 } },
-    { galaxy = "MilkyWay", site = "Moon", address = { 9, 1, 3, 6, 15, 4, 25, 27, 0 }, only_from = { "Earth" }, only_to = {"Earth"} },
+    { galaxy = "MilkyWay", site = "Moon", address = { 6, 7, 27, 31, 23, 18, 3, 5, 0 }, only_from = { "Earth" }, only_to = {"Earth"} },
     { galaxy = "MilkyWay", site = "Abydos", address = { 26, 6, 14, 31, 11, 29, 0 } },
     { galaxy = "MilkyWay", site = "Nether", address = { 27, 23, 4, 34, 12, 28, 0 } },
     { galaxy = "MilkyWay", site = "End", address = { 9, 2, 1, 31, 34, 29, 16, 24, 0 } },
