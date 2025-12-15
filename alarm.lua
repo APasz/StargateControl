fe433f7d6fc280a369d96c427fa5f836101ac036
APasz <a.pasz96@gmail.com>
2025-12-15 17:05:09 +1100

    Sync

--- alarm.lua
+++ alarm.lua
@@ -9,8 +9,8 @@ local DEFAULT_SETTINGS_CONTENT = [[return {
     phase_sides = { "left", "top", "right" },
     flash_delay = 0.28,
     status_flash_duration = 0.25,
-    status_flash_interval = 0.75
-    debounce_reads = 1
+    status_flash_interval = 0.75,
+    debounce_reads = 1,
 }
 ]]
 
