ee42e5009dff3afdccaf7dba14bbaed2c436d332
APasz <a.pasz96@gmail.com>
2025-12-25 10:03:20 +1100

    End <> Lantea

--- addresses.lua
+++ addresses.lua
@@ -1,11 +1,12 @@
 return {
-    { galaxy = "MilkyWay", intergalaxial = {"*"}, site = "Earth", address = { 30, 18, 9, 5, 25, 14, 31, 15, 0 } },
-    { galaxy = "MilkyWay", site = "Moon", address = { 6, 7, 27, 31, 23, 18, 3, 5, 0 }, only_from = { "Earth" }, only_to = {"Earth"} },
+    { galaxy = "MilkyWay", intergalaxial = { "*" }, site = "Earth", address = { 30, 18, 9, 5, 25, 14, 31, 15, 0 } },
+    { galaxy = "MilkyWay", site = "Moon", address = { 6, 7, 27, 31, 23, 18, 3, 5, 0 }, only_from = { "Earth" }, only_to = { "Earth" } },
     { galaxy = "MilkyWay", site = "Abydos", address = { 26, 6, 14, 31, 11, 29, 0 } },
     { galaxy = "MilkyWay", site = "Nether", address = { 27, 23, 4, 34, 12, 28, 0 } },
-    { galaxy = "MilkyWay", site = "End", intergalaxial = {"Lantea", "Lantea_System"}, address = { 9, 2, 1, 31, 34, 29, 16, 24, 0 } },
+    { galaxy = "MilkyWay", site = "End", intergalaxial = { "Lantea_System",  }, address = { 9, 2, 1, 31, 34, 29, 16, 24, 0 } },
     { galaxy = "MilkyWay", site = "Mars", address = { 32, 10, 19, 20, 17, 2, 24, 21, 0 } },
     { galaxy = "MilkyWay", site = "Vermilion", address = { 13, 3, 17, 2, 14, 21, 32, 1, 0 }, only_from = { "Earth" }, hide_on = { "Earth" } },
-    { galaxy = "Pegasus", intergalaxial = {"Earth"}, site = "Lantea_System", address = { 18, 20, 1, 15, 14, 7, 19, 0 }, hide_on = { "Lantea" } },
-    { galaxy = "Pegasus", intergalaxial = {"End"}, site = "Lantea", address = { 9, 27, 26, 14, 21, 1, 4, 3, 0 }, hide_on = { "End" } },
+    { galaxy = "Pegasus", intergalaxial = { "Earth", "End" }, site = "Lantea_Galaxy", address = { 18, 20, 1, 15, 14, 7, 19, 0 }, hide_on = { "Lantea" } },
+    { galaxy = "Pegasus", intergalaxial = { "End" }, site = "Lantea", address = { 9, 27, 26, 14, 21, 1, 4, 3, 0 } },
+    { galaxy = "Pegasus", intergalaxial = { "End" }, site = "Lantea_System", address = { 29, 5, 17, 34, 6, 12, 0 }, hide_on = { "Lantea" } },
 }
